9bac59b8d844d1fa6bee80985a1583d8fea4b840
cao5zy <cao5zy@163.com>
2016-11-29 11:47:41 +0800

    将component转换成了directive

---
+++ src/lab/src/modules/lab-module1/component/bootstrap-popover.directive.ts
@@ -0,0 +1,26 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: '[lab-bootstrap-popover]'
+	,templateUrl: './../template/lab-bootstrap-popover.template.html'
+	,styleUrls: []
+})
+export class BootstrapPopoverDirective implements OnInit {
+	private content:string = "";
+
+	ngOnInit(){
+		$('#popover1').popover({
+			html:true
+			,content:():string=>{
+				return this.content;
+			}
+		});
+	}
+
+	//当完成数据绑定时，再调用这个方法
+	loadPopover(cont:string = ""){
+		this.content = cont;
+		
+	}
+
+}
